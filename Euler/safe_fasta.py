3e8679f860303a397f154afd541ed7a94eb9c516
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-16 14:25:35 +0500

    Update and rename create_fasta.py to safe_fasta.py

--- Euler/safe_fasta.py
+++ Euler/safe_fasta.py
@@ -1,4 +1,4 @@
-def create_fasta(file_name, seq_dict):               # функция записывает идентификатор и последовательность каждого элемента из словаря в файл
+def safe_fasta(file_name, seq_dict):               # функция записывает идентификатор и последовательность каждого элемента из словаря в файл
     with open(file_name, 'w') as file:
         for seq_id, seq in seq_dict.items():
             file.write('>' + seq_id + '\n')           # элемент начнется с символа >, один на строку
