92693e11c8435444292185743cdb1aac77e1e6ca
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-16 14:23:21 +0500

    Update exceptions_started_point.py

--- Euler/exceptions_started_point.py
+++ Euler/exceptions_started_point.py
@@ -1,1 +1,10 @@
+def can_build_de_bruijn_graph(edges):
+    degrees = defaultdict(int)
+    for (a, b), count in edges.items():
+        degrees[a] -= count
+        degrees[b] += count
 
+    start_nodes = sum(1 for degree in degrees.values() if degree < 0)
+    end_nodes = sum(1 for degree in degrees.values() if degree > 0)
+
+    return start_nodes <= 1 and end_nodes <= 1
