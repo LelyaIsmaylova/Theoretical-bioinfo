f85d7294172e7f6523dee40c08913ae076f4ff82
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-16 12:00:15 +0500

    Update main.py

--- Euler/main.py
+++ Euler/main.py
@@ -1,1 +1,13 @@
+print('Enter title \n >')
+title = str(input())
 
+print('Enter file name ')
+file_name = str(input())
+
+if file_name == '': 
+    file_name = 'New file'
+  
+if title == '': 
+    title = head + ' assembled '
+
+create_fasta(file_name, genome, title )
