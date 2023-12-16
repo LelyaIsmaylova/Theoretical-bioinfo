6a84edf6eaa2f7044c79fd06e57a19ed305706c6
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-16 16:03:53 +0500

    Update validate_fasta.py

--- Euler/validate_fasta.py
+++ Euler/validate_fasta.py
@@ -1,8 +1,10 @@
 def validate_fasta(sequences, k):
     valid_nucleotides = {'A', 'T', 'G', 'C'}
     for seq in sequences:
-        if len(seq) != k:
-            return False, f"Последовательность '{seq}' не является строкой длины {k}."
+        if not all(nucleotide in valid_nucleotides for nucleotide in seq) and len(seq) != k:
+            return False, f"Последовательность '{seq}' совсем плоха (содержит недопустимые элементы И не является строкой длины {k})."
         if not all(nucleotide in valid_nucleotides for nucleotide in seq):
             return False, f"Последовательность '{seq}' содержит недопустимые элементы (буквы)."
+        if len(seq) != k:
+            return False, f"Последовательность '{seq}' не является строкой длины {k}."
     return True, "Проверка прошла успешно."
