53f0005341093b7e121b686d2845e71c27b75514
LelyaIsmaylova <92105503+LelyaIsmaylova@users.noreply.github.com>
2023-12-16 14:21:33 +0500

    Update de_Bruijn_graph.py

--- Euler/de_Bruijn_graph.py
+++ Euler/de_Bruijn_graph.py
@@ -1,23 +1,8 @@
-def de_Bruijn_graph(readings):
-    pre = [readings[0][0:len_readings-1]]
-    suf = [readings[0][1:]]
-    nodes = []
-    links = []
-  
-    for i in range(1, number_of_readings): 
-        pre.append(readings[i][0:len_readings-1])
-        suf.append(readings[i][1:])
-      
-        for j in range(len(pre)): 
-            if pre[i] == suf[j] and i != j :
-
-                nodes.append(pre[i])
-                links.append((j, i))
-              
-            if suf[i] == pre[j] and i!= j:
-                nodes.append(pre[j])
-                links.append((i,j))
-              
-    return(nodes, links)
-  
-nodes, links = de_Bruijn_fraph(readings)
+def build_de_bruijn_graph(sequences, k):
+    edges = defaultdict(int)
+    for seq in sequences:
+        for i in range(len(seq) - k + 1):
+            kmer1 = seq[i:i+k-1]
+            kmer2 = seq[i+1:i+k]
+            edges[(kmer1, kmer2)] += 1
+    return edges
